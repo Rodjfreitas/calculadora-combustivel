2cd2b192bfefd0efbc536bda5e718ac803eb77b2
Rodjfreitas <rodrigofreitas2011@live.com>
2023-01-30 15:45:59 -0300

    feat: line of code for calculations

--- js/javascript.js
+++ js/javascript.js
@@ -13,3 +13,32 @@ function imageOut(){
   image.style.transition = 'all 0.8s ease-in-out'
    
 }
+
+function Reset(){
+  location.reload()
+}
+
+function Calcular(){
+  var price = document.querySelector('#price')
+  var kmh = document.querySelector('#kmh')
+  var valor = document.querySelector('#money')
+  if(valor.value == 0){
+    var erro = document.querySelector('.erro')
+    erro.innerText = 'Preencha o valor que será abastecido'
+  }else{
+    var telaConteudo = document.querySelector('#content')
+    var telaResultado = document.querySelector('#resultado')
+    telaConteudo.style.display = 'none'
+    telaResultado.style.display = 'block'
+
+    var litros = document.querySelector('.litros')
+    var valorLitros = Number(valor.value) / Number(price.value)
+   litros.innerHTML = `<h2>R$ ${Number(valor.value)} abastecerá 
+    <strong>${Number(valorLitros)}</strong> litros de combustível`
+
+    var kmLitros = document.querySelector('.kmLitros')
+    kmLitros.innerHTML = `<h2>Sua autonomia será de <strong>
+    ${Number(valorLitros) * Number(kmh.value)}</strong> km
+    de combustível.`
+    }
+}
